709969967cc9c91ef62c6237d5fdff0e770d52ce
shuvo794 <shuvohossain794@gmail.com>
2022-01-19 23:20:32 +0600

    change

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 5000;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8g1rh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-
+console.log(uri);
 
 async function run() {
   try {
