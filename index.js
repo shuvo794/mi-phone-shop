b6b858a1213b1e05e5e19a5aed52e908f65b44a2
shuvo794 <shuvohossain794@gmail.com>
2023-01-23 19:58:02 +0600

    update code

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 const port = process.env.PORT || 5000;
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bvkvy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-// const client = new MongoClient(uri);
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8g1rh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
