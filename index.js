2cdc4279011e5d91fd04a387c3d9c943c17c54a9
shuvo794 <shuvohossain794@gmail.com>
2023-01-24 08:40:03 +0600

    update

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ app.use(express.json());
 app.use(cors());
 const port = process.env.PORT || 5000;
 
+/* const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8g1rh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true }); */
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8g1rh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
